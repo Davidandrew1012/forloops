5de13112fc67a98fe50f77b89e123f888e98ce48
david01relic <davidandrew1012@gmail.com>
2023-02-21 15:22:04 -0500

    e19-e20

--- exercises/e20.js
+++ exercises/e20.js
@@ -6,8 +6,16 @@
 // NOTE: You MUST use double/nested FOR loop to solve this exercise. The array.includes() method is NOT allowed.
 
 export function separateNamesWithAFromRest(array) {
-  // Your code goes here...
-
+  let namesWithA = [];
+  let namesWithoutA = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].includes("a") || array[i].includes("A")) {
+      namesWithA.push(array[i]);
+    } else {
+      namesWithoutA.push(array[i]);
+    }
+  }
+  return [namesWithA, namesWithoutA];
 }
 
 
